ea145af3cc375215eebd96155dcbccc83d00aa3b
valentinfuntak <valentinfuntakk@gmail.com>
2025-02-23 12:05:05 +0100

    Automatization 23.2.2025

---
+++ src/Auto/register-account.js
@@ -0,0 +1,20 @@
+import puppeteer from 'puppeteer';
+
+const browser = await puppeteer.launch({ headless: false });
+const page = await browser.newPage();
+
+// Navigate the page to a URL.
+await page.goto('https://accounts.google.com/signup');
+
+// Set screen size.
+await page.setViewport({ width: 1080, height: 1024 });
+
+// Simuliraj tipkanje za "Ime" s kašnjenjem između svakog slova
+await page.type('#firstName', 'Ime', { delay: 100 }); 
+await page.type('#lastName', 'Prezime', { delay: 100 });  
+
+console.log('Ime i Prezime su upisani.');
+
+await page.click('#collectNameNext > div > button > span');
+
+await browser.close();
