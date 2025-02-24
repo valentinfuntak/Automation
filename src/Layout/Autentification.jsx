bd60b5b2d54a9dc217515e4b865f30005cdfbb8f
valentinfuntak <valentinfuntakk@gmail.com>
2025-02-24 19:54:18 +0100

    Updates

--- src/Layout/Autentification.jsx
+++
@@ -1,9 +0,0 @@
-export default function RegLogLayout(props) {
-    return (
-        <>
-            <div class="min-h-screen w-full bg-gray-900 p-4 py-6 lg:py-8">
-                <div class="mx-auto w-full max-w-screen-xl">{props.children}</div>
-            </div>
-        </>
-    );
-}
--- src/Pages/Settings.jsx
+++
@@ -1,9 +0,0 @@
-export default function Settings() {
-    return (
-      <>
-        <div className="hero min-h-screen bg-base-100 flex flex-col items-center justify-center text-center p-4">
-          <h1 className="text-4xl text-white sm:text-5xl font-bold">Settings</h1>
-        </div>
-      </>
-    );
-  }
