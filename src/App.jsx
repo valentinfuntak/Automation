ccbe1cb2f58d24c832b14802647d578744609025
valentinfuntak <valentinfuntakk@gmail.com>
2025-04-01 08:20:56 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { AuthProvider } from "../src/Auth/AuthProvider.jsx";
 import MainLayout from "../src/Layout/MainLayout.jsx";
 import Auth from "../src/Layout/Autentification.jsx";
 
+import Error from "../src/Pages/Error.jsx"
 import SignUp from "../src/Pages/SignUp.jsx"
 import SignIn from "../src/Pages/SignIn.jsx"
 import Home from "../src/Pages/Home.jsx"
@@ -18,6 +19,7 @@ export default function App() {
   return (
     <AuthProvider>
       <HashRouter>
+      <Route path="*" component={() => (<Error />)} />
         <Route path="/" component={() => (<Auth><SignUp /></Auth>)} />
         <Route path="/SignIn" component={() => (<Auth><SignIn /></Auth>)} />
         <Route path="/Home" component={() => (<MainLayout><Home /></MainLayout>)} />
